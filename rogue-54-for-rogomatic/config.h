fc851ff04d80099ac0b8db1616ade8ede719fa47
Christine Tham <chris.tham@hellotham.com>
2026-06-07 14:22:58 +1000

    feat(rogue): update Makefile.wasm and config.h for standalone build

---
+++ rogue-54-for-rogomatic/config.h
@@ -0,0 +1,27 @@
+/* config.h for WASM build */
+
+#define HAVE_ARPA_INET_H 1
+#define HAVE_FCNTL_H 1
+#define HAVE_LIMITS_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_SYS_IOCTL_H 1
+#define HAVE_TERMIOS_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_CURSES_H 1
+#define HAVE_MATH_H 1
+
+#define HAVE_ERASECHAR 1
+#define HAVE_KILLCHAR 1
+#define HAVE_ALARM 1
+#define HAVE_MEMSET 1
+#define HAVE_SETENV 1
+#define HAVE_STRCHR 1
+#define HAVE_STRERROR 1
+
+#define PROGRAM "rogue"
+#define SCOREFILE "rogue.scr"
+#define LOCKFILE "rogue.lck"
+#define ALLSCORES 1
+#define NUMSCORES 10
+#define NUMNAME "Ten"
